95eed4740c28d2d849393bfc6c18513290e6d861
wubodanran <wubodanran@163.com>
2019-06-28 18:12:29 +0800

    更新包名.

---
+++ bui-dome-x/app/src/main/java/com/dan/dome/SplashActivity.java
@@ -0,0 +1,75 @@
+package com.dan.dome;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.dan.dome.activity.LoginActivity;
+import com.dan.library.androidx.util.PermissionsUtil;
+import com.dan.library.util.StatusBarUtils;
+import com.xuexiang.xutil.tip.ToastUtils;
+
+/**
+ * Created by Dan on 2019/1/17 15:30
+ * 启动页
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    //两秒后进入系统
+    private final int SPLASH_DISPLAY_TIME = 2000;
+    private boolean testFlag = false;
+    private int permissionSize = 0;
+    private int maxPermissionSize = 7;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //设置状态栏全屏
+        StatusBarUtils.setWindowFullScreen(this);
+
+        setContentView(R.layout.activity_splash);
+
+        permissions();
+    }
+
+    private void init() {
+        Intent intent;
+        if (testFlag) {
+            intent = new Intent(getApplicationContext(), MainActivity.class);
+        } else {
+            intent = new Intent(getApplicationContext(), LoginActivity.class);
+        }
+        SplashActivity.this.startActivity(intent);
+        SplashActivity.this.finish();
+    }
+
+    public void permissions() {
+        String[] permissionsList = new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        PermissionsUtil permissionsUtil = new PermissionsUtil(this);
+        if (!permissionsUtil.checkPermission(permissionsList)) {
+            permissionSize++;
+            permissionsUtil.getPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, permissionsList);
+        } else {
+            //停留2秒然后进主页面
+            new android.os.Handler().postDelayed(this::init, SPLASH_DISPLAY_TIME);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            init();
+        } else {
+            ToastUtils.toast("请开通相关权限，否则无法正常使用本应用！");
+            if (permissionSize < maxPermissionSize) {
+                permissions();
+            }
+        }
+    }
+
+}
